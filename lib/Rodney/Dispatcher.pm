21d4100ec57c8bfee74905ed568c9ce057711e1f
Shawn M Moore <sartak@gmail.com>
2008-01-27 12:14:02 -0600

    Add the Dispatcher
    darcs-hash:20080127181402-aa95a-eab814892ceb5a1ca8ad88fb9fc0254eb5258c65.gz

---
+++ lib/Rodney/Dispatcher.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+package Rodney::Dispatcher;
+use strict;
+use warnings;
+use Rodney::Command::Gamesby;
+use Rodney::Command::Rot13;
+
+sub on;
+
+on qr{^!gamesby\b}i => sub { Rodney::Command::Gamesby->run(@_) };
+on qr{^!rot13\s+(.*)}i => sub { Rodney::Command::Rot13->run(@_, text => $1) };
+
+my @rules;
+sub on {
+    my ($re, $code) = @_;
+    push @rules, [$re, $code];
+}
+
+sub dispatch {
+    my $self = shift;
+    my $args = shift;
+    local $_ = $args->{body};
+
+    for my $rule (@rules) {
+        my ($re, $code) = @$rule;
+        if ($_ =~ $re) {
+            return $code->($args);
+            last;
+        }
+    }
+
+    return;
+}
+
+1;
+
