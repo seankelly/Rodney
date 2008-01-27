074cf8f4e2ca07a04299049bb8dc0568b26edf62
Shawn M Moore <sartak@gmail.com>
2008-01-27 12:13:11 -0600

    Dispatch commands a la Jifty, various other small changes
    darcs-hash:20080127181311-aa95a-46c9efb9238fbf75a361e72d1ce8137e779cc2b1.gz

--- lib/Rodney.pm
+++ lib/Rodney.pm
@@ -3,11 +3,28 @@ package Rodney;
 use strict;
 use warnings;
 use parent 'Bot::BasicBot';
+use Rodney::Dispatcher;
+use Module::Refresh;
 
 sub said {
-    my ($self, $args) = @_;
+    my $self = shift;
+    my $args = shift;
+
     print "<$args->{who}> $args->{body}\n";
-    return '';
+
+    my $ret = eval { $self->dispatch($args) };
+    warn $@ if $@;
+
+    return $ret;
+}
+
+sub dispatch {
+    my $self = shift;
+    my $args = shift;
+
+    Module::Refresh->refresh;
+
+    return Rodney::Dispatcher->dispatch($args);
 }
 
 1;
