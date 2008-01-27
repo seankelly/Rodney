1c5088e576b41d2e9fccfb290dea17303643de13
Shawn M Moore <sartak@gmail.com>
2008-01-27 14:41:56 -0600

    Connect to the database on ->new, add handle to the command arguments
    darcs-hash:20080127204156-aa95a-94710a09d726caf6ea4ee88a91a49139a2caea15.gz

--- lib/Rodney.pm
+++ lib/Rodney.pm
@@ -5,12 +5,27 @@ use warnings;
 use parent 'Bot::BasicBot';
 use Rodney::Dispatcher;
 use Module::Refresh;
+use Jifty::DBI::Handle;
+
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+
+    $self->{handle} = Jifty::DBI::Handle->new;
+    $self->{handle}->connect(
+        driver => 'SQLite',
+        database => 'nethack',
+    );
+
+    return $self;
+}
 
 sub said {
     my $self = shift;
     my $args = shift;
 
     print "<$args->{who}> $args->{body}\n";
+    $args->{handle} = $self->{handle};
 
     my $ret = eval { $self->dispatch($args) };
     warn $@ if $@;
