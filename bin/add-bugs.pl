e5ef5dc1308e7d0f942ae14f9c1fdfa0f13077e5
Sean <rodney4@katron.org>
2009-03-24 23:42:04 -0500

    Convert bin/add-bugs to Fey

--- bin/add-bugs.pl
+++ bin/add-bugs.pl
@@ -2,59 +2,47 @@
 use strict;
 use warnings;
 use lib 'lib';
-use Rodney::Bug;
 use LWP::Simple;
+use Rodney::Model::Bug;
+use HTML::TreeBuilder;
 
-use Jifty::DBI::Handle;
-my $handle = Jifty::DBI::Handle->new;
-$handle->connect(
-    driver => 'SQLite',
-    database => 'nethack',
+my @urls = (
+    'http://www.nethack.org/v343/bugs.html',
+    'http://www.nethack.org/v343/spoiler.html',
 );
 
-my $bug_content = get('http://www.nethack.org/v343/bugs.html') . get('http://www.nethack.org/v343/spoiler.html');
-die "Couldn't get bugs" unless defined $bug_content;
+for my $url (@urls) {
+    my $content = get($url);
+    die "Could not get $url" unless defined $content;
+
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($content);
+    $tree->eof;
 
 
-while ($bug_content =~ s{
-<tr>\s*
-
-<td>
-<a\ name="[^"]+">
-  ([^<]+?)
-  \s*
-</a>
-\s*
-(?:</td>)?
-<td>
-<a\ href="[^"]+">
-  ([^<]+?)
-  \s*
-</a>
-\s*
-
-(?:</td>)?
-<td>
-([^<]+?)
-\s*
-
-(?:</td>)?
-<td>
-.*?
-
-(?:</td>)?(?:</tr>)?
-}{}xms) {
-    my ($id, $status, $text) = ($1, $2, $3);
-    $text =~ s/\n/ /g;
-
-    $status = 'NextVersion' if $status eq 'Fixed';
-
-    my %create = (
-        bugid       => $id,
-        status      => $status,
-        description => $text,
+    my @bugs = $tree->look_down(
+        _tag => 'tr',
+        sub { my @tags = $_[0]->find('td'); @tags >= 3 },
     );
 
-    my $bug = Rodney::Bug->new(handle => $handle);
-    $bug->create(%create);
+    for my $bug (@bugs) {
+        my ($id, $status, $desc) = $bug->find('td');
+        $id = $id->as_text;
+        $status = $status->as_text;
+        $desc = $desc->as_text;
+
+        my $Bug = Rodney::Model::Bug->new(bugid => $id);
+        unless (defined $Bug) {
+            # This is to ameliorate confusion between
+            # 'fixed' and 'Fixed'.
+            $status = 'NextVersion' if $status eq 'Fixed';
+            Rodney::Model::Bug->insert(
+                bugid       => $id,
+                status      => $status,
+                description => $desc,
+            );
+        }
+    }
+
+    $tree->delete;
 }
