2d4f8651070939e64943f4306709e592da3844f5
sean <sean@fenring.local>
2009-03-30 14:02:15 -0500

    Convert !bug(s) command over to new system
    Right now it's just a shell, but it should not be that difficult
    to add the basic functionality.

---
+++ lib/Rodney/Command/Bug.pm
@@ -0,0 +1,19 @@
+package Rodney::Command::Bug;
+use Moose;
+extends 'Rodney::Command';
+
+use Rodney::Model::Bug;
+
+our @COMMANDS = qw/bug bugs/;
+
+sub help {
+    return 'Help text for the bugdb';
+}
+
+sub run {
+    my $self = shift;
+    my $args = shift;
+}
+
+1;
+
