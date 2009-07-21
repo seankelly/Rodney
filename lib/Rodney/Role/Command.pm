78d2785427044c13dd0a5b4bdc0fb4f29c077a5b
Sean <rodney4@katron.org>
2009-07-21 00:30:29 -0500

    Begin the turning of commands into a plugin+role

--- lib/Rodney/Role/Command.pm
+++ lib/Rodney/Role/Command.pm
@@ -1,6 +1,10 @@
 package Rodney::Role::Command
 use Moose::Role;
 
+# Command method returns a list of commands.
+# Run is what's used to run the command.
+requires qw/command run/;
+
 no Moose::Role;
 
 1;
