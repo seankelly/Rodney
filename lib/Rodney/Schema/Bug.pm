a98b7b85b336d9b70863dd551c9d7361209228d8
Sean <rodney4@katron.org>
2009-08-18 17:59:06 -0500

    Set the primary key for R::S::Bug

--- lib/Rodney/Schema/Bug.pm
+++ lib/Rodney/Schema/Bug.pm
@@ -6,5 +6,6 @@ use base qw/DBIx::Class/;
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('bug');
 __PACKAGE__->add_columns(qw/bugid status description/);
+__PACKAGE__->set_primary_key('bugid');
 
 1;
